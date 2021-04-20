43eb941a8efaa458ef2f1b783019c6b7b7a1606b
andresgumo <AndresGuacheta>
2021-04-20 13:06:27 -0500

    enrutamiento amigos

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -8,7 +8,7 @@ const NavbarComp=()=>{
         <ul class="navigation">
             <li class="navItem" > <Link to="/" >RESEÑAS</Link></li>
             <li class="navItem"> <Link to="/inventory">INVENTARIO</Link> </li>
-            <li class="navItem"> <Link to="/">AMIGOS</Link></li>
+            <li class="navItem"> <Link to="/Community">AMIGOS</Link></li>
             <li class="navItem"> <Link to="/error">PERFIL</Link></li>
         </ul>
     </nav>
